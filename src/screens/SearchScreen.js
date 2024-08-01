e13c9f2d7db5a27c8e3386a00b1a9319ff8e8000
Oliver Sánchez <oliversancheze@gmail.com>
2024-08-01 16:43:34 -0500

    pantalla de busqueda

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,84 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from "react";
+import { View, ScrollView } from "react-native";
+import { SearchBar, ListItem, Avatar, Icon, Text } from "react-native-elements";
+import {
+  collection,
+  query,
+  startAt,
+  endAt,
+  limit,
+  orderBy,
+  getDocs,
+} from "firebase/firestore";
+import { size, map } from "lodash";
+import { useNavigation } from "@react-navigation/native";
+import { Loading } from "../components/Shared";
+import { db, screen } from "../utils";
 
 export function SearchScreen() {
+  const [searchText, setSearchText] = useState("");
+  const [searchResults, setSearchResults] = useState(null);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    (async () => {
+      const q = query(
+        collection(db, "lugares"),
+        orderBy("name"),
+        startAt(searchText),
+        endAt(`${searchText}\uf8ff`),
+        limit(20)
+      );
+
+      const querySnapshot = await getDocs(q);
+      setSearchResults(querySnapshot.docs);
+    })();
+  }, [searchText]);
+
+  const goToLugar = (idLugar) => {
+    navigation.navigate(screen.lugar.tab, {
+      screen: screen.lugar.lugar,
+      params: {
+        id: idLugar,
+      },
+    });
+  };
+
   return (
-    <View>
-      <Text>Estamos en la vista de busqueda</Text>
-    </View>
-  )
+    <>
+      <SearchBar
+        placeholder="Busca tu lugar"
+        value={searchText}
+        onChangeText={(text) => setSearchText(text)}
+      />
+
+      {!searchResults && <Loading show text="Cargando" />}
+
+      <ScrollView>
+        {size(searchResults) === 0 ? (
+          <View style={{ alignItems: "center", marginTop: 20 }}>
+            <Text>No se han encontrado resultados</Text>
+          </View>
+        ) : (
+          map(searchResults, (item) => {
+            const data = item.data();
+
+            return (
+              <ListItem
+                key={data.id}
+                bottomDivider
+                onPress={() => goToLugar(data.id)}
+              >
+                <Avatar source={{ uri: data.images[0] }} rounded />
+                <ListItem.Content>
+                  <ListItem.Title>{data.name}</ListItem.Title>
+                </ListItem.Content>
+                <Icon type="material-community" name="chevron-right" />
+              </ListItem>
+            );
+          })
+        )}
+      </ScrollView>
+    </>
+  );
 }
